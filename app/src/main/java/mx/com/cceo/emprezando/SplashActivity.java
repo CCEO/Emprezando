78ed89e2628f51f77f3923cd732ef30098d71878
Hugo Medina <rhadammanthis@gmail.com>
2015-10-01 09:48:50 -0500

    Task 1
    Initial project upload. Splash screen with logo and 3 seconds delay

---
+++ app/src/main/java/mx/com/cceo/emprezando/SplashActivity.java
@@ -0,0 +1,60 @@
+package mx.com.cceo.emprezando;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Hides action bar
+        //getActionBar().hide();
+
+        // The thread sleeps for 3 seconds then loads MainActivity
+        Thread logoTimer = new Thread() {
+
+            public void run() {
+                try {
+                    sleep(3000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+            }
+
+        };
+        logoTimer.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
