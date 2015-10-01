eff038a50b66f1d59376a04c9ce856b0e207432a
Hugo Medina <rhadammanthis@gmail.com>
2015-10-01 15:13:24 -0500

    Task 2
    Home Screen tab holder design

--- app/src/main/java/mx/com/cceo/emprezando/MainActivity.java
+++ app/src/main/java/mx/com/cceo/emprezando/MainActivity.java
@@ -1,16 +1,33 @@
 package mx.com.cceo.emprezando;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import mx.com.cceo.emprezando.Fragment.FragmentBlank;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+
+    private static final int NUM_PAGES = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPager = (ViewPager) findViewById(R.id.main_pager);
+        mPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
     }
 
     @Override
@@ -33,5 +50,43 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        } else {
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class MainPagerAdapter extends FragmentStatePagerAdapter {
+
+        public MainPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position)
+            {
+                case 0: return new FragmentBlank();
+                case 1: return new FragmentBlank();
+                case 2: return new FragmentBlank();
+                case 3: return new FragmentBlank();
+                default: return new FragmentBlank();
+            }
+
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
